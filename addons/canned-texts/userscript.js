4cb48f24b17b5855df590fe681132db4c9765bf8
ahmetlii <ahmetlii@users.noreply.github.com>
2020-11-25 11:09:25 +0300

    create addon (js code is coming soon)

---
+++ addons/canned-texts/userscript.js
@@ -0,0 +1,9 @@
+export default async function ({ addon, global, console }) {
+    if (!addon.tab.clientVersion) return;
+    console.log("Canned texts are enabled.")
+    const a = document.createElement("a");
+    button.textContent = addon.settings.get("buttonName");
+    link.appendChild(a);
+    //<--!coming soon-->
+}
+  
